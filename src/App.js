93e42baf91d3b889de5c58b293d63da2b1b2d2a0
Vanessa Wang <sanguliupo@gmail.com>
2019-02-01 11:20:07 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-     fetch('http://localhost:3000/imageurl', {
+     fetch('https://salty-ridge-15154.herokuapp.com/imageurl', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response=>response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://salty-ridge-15154.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
 	};
 
 	onRegister = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://salty-ridge-15154.herokuapp.com/register', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
