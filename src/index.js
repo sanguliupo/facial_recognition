16990ca522f21f570abf75e9dc3488cc6d771aa2
Vanessa Wang <sanguliupo@gmail.com>
2019-02-01 21:50:50 -0800

    rename tachyons import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import 'tachyons' from 'tachyons/src/tachyons.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
